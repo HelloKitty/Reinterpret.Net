15a660e2c6a2dce0b98fb5ee9a591f84eb6e07d8
HelloKitty <andrew.blakely@ymail.com>
2017-09-17 10:25:39 -0500

    Created a generic base test class for primitives

---
+++ test/Reinterpret.Net.NetFramework.Tests/PrimitiveGenericReinterpretTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Fasterflect;
+using NUnit.Framework;
+
+namespace Reinterpret.Net.NetFramework.Tests
+{
+	[TestFixture]
+	public abstract class PrimitiveGenericReinterpretTest<TTypeToTest>
+		where TTypeToTest : struct, IComparable, IFormattable, IComparable<TTypeToTest>, IEquatable<TTypeToTest>
+	{
+		public static IEnumerable<TTypeToTest> ValuesToTest { get; }
+
+		static PrimitiveGenericReinterpretTest()
+		{
+			int sizeOfValue = Marshal.SizeOf<TTypeToTest>();
+
+			ValuesToTest = GetPowerOfTwoRange(GetMaxValue(), 0, sizeOfValue * 8)
+				.Concat(GetPowerOfTwoRange(GetMinValue(), 0, sizeOfValue * 8))
+				.Select(l => (TTypeToTest)Convert.ChangeType(l, typeof(TTypeToTest)))
+				.ToArray();
+		}
+
+		[Test]
+		[TestCaseSource(nameof(ValuesToTest))]
+		public void TestCanReinterpretToInt32(TTypeToTest valueToTest)
+		{
+			//arrange
+			//We abuse the DLR so that we can keep this generic
+			byte[] bytes = BitConverter.GetBytes((dynamic)valueToTest);
+
+			//act
+			TTypeToTest value = bytes.Reinterpret<TTypeToTest>();
+
+			//assert
+			Assert.AreEqual(valueToTest, value, $"Result from reinterpret cast was not the same.");
+		}
+
+		private static Int64 GetMaxValue()
+		{
+			Type convertType = typeof(TTypeToTest);
+
+			FieldInfo field = convertType.GetField("MaxValue", BindingFlags.Static | BindingFlags.Public);
+
+			return ConvertToType<Int64>(field.GetRawConstantValue());
+		}
+
+		private static Int64 GetMinValue()
+		{
+			Type convertType = typeof(TTypeToTest);
+
+			FieldInfo field = convertType.GetField("MinValue", BindingFlags.Static | BindingFlags.Public);
+
+			return ConvertToType<Int64>(field.GetRawConstantValue());
+		}
+
+		private static TTypeToTest ConvertToType(object value)
+		{
+			return (TTypeToTest) Convert.ChangeType(value, typeof(TTypeToTest));
+		}
+
+		private static TTypeToConvertTo ConvertToType<TTypeToConvertTo>(object value)
+		{
+			return (TTypeToConvertTo)Convert.ChangeType(value, typeof(TTypeToConvertTo));
+		}
+
+		protected static IEnumerable<Int64> GetPowerOfTwoRange(Int64 mainValue, int start, int stop)
+		{
+			return Enumerable.Range(start, stop)
+				.Select(i => mainValue / (Int64)Math.Pow(2, i));
+		}
+	}
+}
--- test/Reinterpret.Net.NetFramework.Tests/PrimitiveReinterpretCastTests.cs
+++ test/Reinterpret.Net.NetFramework.Tests/PrimitiveReinterpretCastTests.cs
@@ -9,32 +9,11 @@ using NUnit.Framework;
 namespace Reinterpret.Net.NetFramework.Tests
 {
 	[TestFixture]
-	public static class PrimitiveReinterpretCastTests
+	public sealed class PrimitiveReinterpretCastTests : PrimitiveGenericReinterpretTest<Int32>
 	{
-		private static IEnumerable<int> int32sToTest = GetPowerOfTwoRange(int.MaxValue, 0, sizeof(int) * 8)
-			.Concat(GetPowerOfTwoRange(int.MinValue, 0, sizeof(int) * 8))
-			.Select(l => (int) l);
-
-
-		[Test]
-		[TestCaseSource(nameof(int32sToTest))]
-		public static void TestCanReinterpretToInt32(int valueToTest)
+		public PrimitiveReinterpretCastTests()
 		{
-
-			//arrange
-			byte[] bytes = BitConverter.GetBytes(valueToTest);
-
-			//act
-			int value = bytes.Reinterpret<int>();
-
-			//assert
-			Assert.AreEqual(valueToTest, value, $"Result from reinterpret cast was not the same.");
-		}
-
-		private static IEnumerable<Int64> GetPowerOfTwoRange(Int64 mainValue, int start, int stop)
-		{
-			return Enumerable.Range(start, stop)
-				.Select(i => mainValue / (Int64) Math.Pow(2, i));
+			
 		}
 	}
 }
