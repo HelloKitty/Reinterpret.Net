ee0fe3092a84d078fc26f15acfd2c81bda1c1bed
HelloKitty <andrew.blakely@ymail.com>
2017-09-17 15:26:31 -0500

    Added ArrayMemoryHacks and Char Implementation

---
+++ src/Reinterpret.Net/Array/ArrayMemoryHack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Reinterpret.Net
+{
+	internal unsafe static class ArrayMemoryHack
+	{
+		[StructLayout(LayoutKind.Explicit)]
+		internal struct Union
+		{
+			[FieldOffset(0)] public byte[] bytes;
+			[FieldOffset(0)] public char[] chars;
+		}
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		internal struct ArrayHeader
+		{
+			public UIntPtr type;
+			public UIntPtr length;
+		}
+
+		internal static UIntPtr BYTE_ARRAY_TYPE { get; }
+
+		static ArrayMemoryHack()
+		{
+			fixed (void* pBytes = new byte[1])
+			{
+				BYTE_ARRAY_TYPE = GetHeaderPointer(pBytes)->type;
+			}
+		}
+
+		internal static ArrayHeader* GetHeaderPointer(void* pBytes)
+		{
+			return (ArrayHeader*)pBytes - 1;
+		}
+
+		public static void ConvertByteTypeToTargetType(this byte[] bytes, UIntPtr targetTypeTypePointer, int sizeOfTargetType)
+		{
+			if(sizeOfTargetType <= 0) throw new ArgumentOutOfRangeException(nameof(sizeOfTargetType));
+
+			fixed (void* pArray = bytes)
+			{
+				var pHeader = GetHeaderPointer(pArray);
+				pHeader->type = targetTypeTypePointer;
+				pHeader->length = (UIntPtr)(bytes.Length / sizeOfTargetType);
+			}
+		}
+
+		public static void ConvertOtherTypeToByteType(void* arrayPointer, int lengthOfCollection, int sizeOfTargetType)
+		{
+			var pHeader = ArrayMemoryHack.GetHeaderPointer(arrayPointer);
+			pHeader->type = ArrayMemoryHack.BYTE_ARRAY_TYPE;
+			pHeader->length = (UIntPtr)(lengthOfCollection / sizeOfTargetType);
+		}
+	}
+}
---
+++ src/Reinterpret.Net/Array/ByteToCharArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Reinterpret.Net
+{
+	//Based on hack from: http://stackoverflow.com/questions/619041/what-is-the-fastest-way-to-convert-a-float-to-a-byte
+	internal static unsafe class ByteToCharArray
+	{
+		private static readonly UIntPtr CHAR_ARRAY_TYPE;
+
+		static ByteToCharArray()
+		{
+			fixed (void* pChars = new char[1])
+			{
+				CHAR_ARRAY_TYPE = ArrayMemoryHack.GetHeaderPointer(pChars)->type;
+			}
+		}
+
+		public static char[] ToCharArrayPerm(this byte[] bytes)
+		{
+			var union = new ArrayMemoryHack.Union() { bytes = bytes };
+			union.bytes.ConvertByteTypeToTargetType(CHAR_ARRAY_TYPE, sizeof(char));
+			return union.chars;
+		}
+
+		private static byte[] ToByteArrayPerm(this char[] charArray)
+		{
+			var union = new ArrayMemoryHack.Union() { chars = charArray };
+
+			fixed (void* cArray = charArray)
+			{
+				ArrayMemoryHack.ConvertOtherTypeToByteType(cArray, charArray.Length, sizeof(char));
+			}
+
+			return union.bytes;
+		}	
+	}
+}
