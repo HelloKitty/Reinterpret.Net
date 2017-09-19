d178c8fa7819761c375da616883f7ff21c0710da
HelloKitty <andrew.blakely@ymail.com>
2017-09-18 23:55:57 -0500

    Copyed tests over to other versions

---
+++ test/Reinterpret.Net.NetFramework45.Tests/StringReinterpretCastTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace Reinterpret.Net.NetFramework.Tests
+{
+	[TestFixture]
+	public class StringReinterpretCastTests
+	{
+		[Test]
+		[TestCase("test")]
+		[TestCase("")]
+		[TestCase(" ")]
+		[TestCase("\t")]
+		[TestCase("Heloooooooooooooooooooooooo")]
+		[TestCase(@"asd89asd98ahsd893598nioasop)*(AJ*(D(*SHDF(H#%*(H*F(Snsf")]
+		public void Test_Can_Reinterpret_To_UTF16_String(string value)
+		{
+			//arrange
+			byte[] bytes = Encoding.Unicode.GetBytes(value);
+
+			//act
+			string result = bytes.ReinterpretToString();
+
+			//assert
+			Assert.AreEqual(value, result, $"Expected: {value} Result: {result}");
+		}
+
+		[Test]
+		[TestCase("test")]
+		[TestCase("")]
+		[TestCase(" ")]
+		[TestCase("\t")]
+		[TestCase("Heloooooooooooooooooooooooo")]
+		[TestCase(@"asd89asd98ahsd893598nioasop)*(AJ*(D(*SHDF(H#%*(H*F(Snsf")]
+		public void Test_Can_Reinterpret_From_UTF16_String(string value)
+		{
+			//arrange
+			byte[] bytes = Encoding.Unicode.GetBytes(value);
+
+			//act
+			byte[] result = value.ReinterpretFromString();
+
+			//assert
+			Assert.AreEqual(bytes.Length, result.Length);
+			for(int i = 0; i < bytes.Length; i++)
+				Assert.AreEqual(bytes[i], result[i], $"Expected: {bytes[i]} Result: {result[i]}");
+		}
+	}
+}
---
+++ test/Reinterpret.Net.Netframework40.Tests/CustomStructReinterpretCastTests.cs
@@ -0,0 +1,274 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reinterpret.Net.NetFramework.Tests
+{
+	[TestFixture]
+	public class CustomStructReinterpretCastTests
+	{
+		[Test]
+		[TestCase('h', 5)]
+		[TestCase('j', int.MinValue)]
+		[TestCase((char)0, int.MaxValue)]
+		public static void Test_Can_Reinterpret_Simple_ValueOnlyStruct(char charValue, int intValue)
+		{
+			//arrange
+			TestCustomStruct1 testStructValue = new TestCustomStruct1() {c = charValue, i = intValue};
+			byte[] buffer = new byte[Marshal.SizeOf(typeof(TestCustomStruct1))];
+			IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(TestCustomStruct1)));
+
+			//act
+			Marshal.StructureToPtr(testStructValue, ptr, false);
+			Marshal.Copy(ptr, buffer, 0, Marshal.SizeOf(typeof(TestCustomStruct1)));
+			TestCustomStruct1 resultStructValue = buffer.Reinterpret<TestCustomStruct1>();
+
+			//assert
+			Assert.NotNull(buffer);
+
+			Assert.AreEqual(testStructValue.i, resultStructValue.i);
+			Assert.AreEqual(testStructValue.c, resultStructValue.c);
+			Marshal.FreeHGlobal(ptr);
+		}
+
+		[Test]
+		[TestCase('h', new int[] {5})]
+		[TestCase('j', new int[] {int.MinValue, 5, 6, 7})]
+		[TestCase((char)0, new int[] {int.MinValue, 99, 5533})]
+		public unsafe static void Test_Can_Reinterpret_CustomStruct_Containing_Array(char charValue, int[] intValues)
+		{
+			//arrange
+			TestCustomStruct2 testStructValue = new TestCustomStruct2() {c = charValue, i = intValues};
+
+			byte[] buffer = new byte[Marshal.SizeOf(typeof(TestCustomStruct2))];
+			IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(TestCustomStruct2)));
+
+			//act
+			Marshal.StructureToPtr(testStructValue, ptr, false);
+			Marshal.Copy(ptr, buffer, 0, Marshal.SizeOf(typeof(TestCustomStruct2)));
+			TestCustomStruct2 resultStructValue = buffer.Reinterpret<TestCustomStruct2>();
+
+			//assert
+			Assert.NotNull(buffer);
+
+			for(int i = 0; i < intValues.Length; i++)
+				Assert.AreEqual(intValues[i], resultStructValue.i[i]);
+			Assert.AreEqual(testStructValue.c, resultStructValue.c);
+			Marshal.FreeHGlobal(ptr);
+		}
+
+		[Test]
+		[TestCase('h', "Test")]
+		[TestCase('j', "hello")]
+		[TestCase((char)0, @"lololodfsidnfsindf8*H*ASDH*ASDHd")]
+		public unsafe static void Test_Can_Reinterpret_CustomStruct_Containing_String(char charValue, string stringValue)
+		{
+			//arrange
+			TestCustomStruct3 testStructValue = new TestCustomStruct3() {c = charValue, s = stringValue};
+
+			byte[] buffer = new byte[Marshal.SizeOf(typeof(TestCustomStruct2))];
+			IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(TestCustomStruct2)));
+
+			//act
+			Marshal.StructureToPtr(testStructValue, ptr, false);
+			Marshal.Copy(ptr, buffer, 0, Marshal.SizeOf(typeof(TestCustomStruct3)));
+			TestCustomStruct3 resultStructValue = buffer.Reinterpret<TestCustomStruct3>();
+
+			//assert
+			Assert.NotNull(buffer);
+			Assert.NotNull(resultStructValue.s);
+			Assert.AreEqual(stringValue, resultStructValue.s);
+			Assert.AreEqual(testStructValue.c, resultStructValue.c);
+			Marshal.FreeHGlobal(ptr);
+		}
+
+		[Test]
+		[TestCase('h', "Test")]
+		[TestCase('j', "hello")]
+		[TestCase((char)0, @"lololodfsidnfsindf8*H*ASDH*ASDHd")]
+		public unsafe static void Test_Can_Reinterpret_CustomStruct_Containing_RefType(char charValue, string stringValue)
+		{
+			//arrange
+			TestCustomStruct4 testStructValue = new TestCustomStruct4() {c = charValue, refType = new TestRefType() {s = stringValue}};
+
+			byte[] buffer = new byte[Marshal.SizeOf(typeof(TestCustomStruct2))];
+			IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(TestCustomStruct2)));
+
+			//act
+			Marshal.StructureToPtr(testStructValue, ptr, false);
+			Marshal.Copy(ptr, buffer, 0, Marshal.SizeOf(typeof(TestCustomStruct4)));
+			TestCustomStruct4 resultStructValue = buffer.Reinterpret<TestCustomStruct4>();
+
+			//assert
+			Assert.NotNull(buffer);
+			Assert.NotNull(resultStructValue.refType);
+			Assert.AreEqual(stringValue, resultStructValue.refType.s);
+			Assert.AreEqual(testStructValue.c, resultStructValue.c);
+			Marshal.FreeHGlobal(ptr);
+		}
+
+		//From struct to bytes
+		[Test]
+		[TestCase('h', 5)]
+		[TestCase('j', int.MinValue)]
+		[TestCase((char)0, int.MaxValue)]
+		public static void Test_Can_Reinterpret_From_ValueOnlyStruct(char charValue, int intValue)
+		{
+			//arrange
+			TestCustomStruct1 testStructValue = new TestCustomStruct1() {c = charValue, i = intValue};
+
+			//act
+			byte[] result = testStructValue.Reinterpret();
+			TestCustomStruct1 resultBackToStruct = result.Reinterpret<TestCustomStruct1>();
+
+			//assert
+			Assert.AreEqual(testStructValue.i, resultBackToStruct.i);
+			Assert.AreEqual(testStructValue.c, resultBackToStruct.c);
+		}
+
+		[Test]
+		[TestCase('h', new int[] {5})]
+		[TestCase('j', new int[] {int.MinValue, 5, 6, 7})]
+		[TestCase((char)0, new int[] {int.MinValue, 99, 5533})]
+		public unsafe static void Test_Can_Reinterpret_From_Containing_Array(char charValue, int[] intValues)
+		{
+			//arrange
+			TestCustomStruct2 testStructValue = new TestCustomStruct2() {c = charValue, i = intValues};
+
+			//act
+			byte[] result = testStructValue.Reinterpret();
+			TestCustomStruct2 resultBackToStruct = result.Reinterpret<TestCustomStruct2>();
+
+			//assert
+			for(int i = 0; i < intValues.Length; i++)
+				Assert.AreEqual(intValues[i], resultBackToStruct.i[i]);
+			Assert.AreEqual(testStructValue.c, resultBackToStruct.c);
+		}
+
+		[Test]
+		[TestCase('h', "Test")]
+		[TestCase('j', "hello")]
+		[TestCase((char)0, @"lololodfsidnfsindf8*H*ASDH*ASDHd")]
+		public unsafe static void Test_Can_Reinterpret_From_Containing_String(char charValue, string stringValue)
+		{
+			//arrange
+			TestCustomStruct3 testStructValue = new TestCustomStruct3() {c = charValue, s = stringValue};
+
+			//act
+			byte[] result = testStructValue.Reinterpret();
+			TestCustomStruct3 resultBackToStruct = result.Reinterpret<TestCustomStruct3>();
+
+			//assert
+			Assert.NotNull(resultBackToStruct.s);
+			Assert.AreEqual(stringValue, resultBackToStruct.s);
+			Assert.AreEqual(testStructValue.c, resultBackToStruct.c);
+		}
+
+		[Test]
+		[TestCase('h', "Test")]
+		[TestCase('j', "hello")]
+		[TestCase((char)0, @"lololodfsidnfsindf8*H*ASDH*ASDHd")]
+		public unsafe static void Test_Can_Reinterpret_From_Containing_RefType(char charValue, string stringValue)
+		{
+			//arrange
+			TestCustomStruct4 testStructValue = new TestCustomStruct4() {c = charValue, refType = new TestRefType() {s = stringValue}};
+
+			//act
+			byte[] result = testStructValue.Reinterpret();
+			TestCustomStruct4 resultBackToStruct = result.Reinterpret<TestCustomStruct4>();
+
+			//assert
+			Assert.NotNull(resultBackToStruct.refType);
+			Assert.AreEqual(stringValue, resultBackToStruct.refType.s);
+			Assert.AreEqual(testStructValue.c, resultBackToStruct.c);
+		}
+
+		[Test]
+		[TestCase('h', 5)]
+		[TestCase('j', int.MinValue)]
+		[TestCase((char)0, int.MaxValue)]
+		public static void Test_Can_Reinterpret_ArrayOf_ValueOnlyStruct(char charValue, int intValue)
+		{
+			//arrange
+			TestCustomStruct1[] testStructValue = new TestCustomStruct1[] { new TestCustomStruct1() { c = charValue, i = intValue }, new TestCustomStruct1() { c = 't', i = 5 } };
+
+			byte[] realBytes = testStructValue
+				.SelectMany(c => c.Reinterpret())
+				.ToArray();
+
+			//act
+			byte[] result = testStructValue.Reinterpret();
+
+			//assert
+			Assert.NotNull(result);
+			Assert.AreEqual(realBytes.Length, result.Length);
+
+			for(int i = 0; i < realBytes.Length; i++)
+			{
+				Assert.AreEqual(realBytes[i], result[i]);
+			}
+		}
+
+		[Test]
+		[TestCase('h', 5)]
+		[TestCase('j', int.MinValue)]
+		[TestCase((char)0, int.MaxValue)]
+		public static void Test_Can_Reinterpret_To_ArrayOf_ValueOnlyStruct(char charValue, int intValue)
+		{
+			//arrange
+			TestCustomStruct1[] testStructValue = new TestCustomStruct1[] { new TestCustomStruct1() { c = charValue, i = intValue }, new TestCustomStruct1() { c = 't', i = 5 } };
+
+			//act
+			TestCustomStruct1[] result = testStructValue
+				.SelectMany(c => c.Reinterpret())
+				.ToArray()
+				.ReinterpretToArray<TestCustomStruct1>();
+
+			//assert
+			Assert.NotNull(result);
+			Assert.AreEqual(testStructValue.Length, result.Length);
+
+			for(int i = 0; i < testStructValue.Length; i++)
+			{
+				Assert.AreEqual(testStructValue[i].i, result[i].i);
+				Assert.AreEqual(testStructValue[i].c, result[i].c);
+			}
+		}
+
+		public class TestRefType
+		{
+			public string s;
+		}
+
+		public struct TestCustomStruct1
+		{
+			public int i;
+			public char c;
+		}
+
+		public unsafe struct TestCustomStruct2
+		{
+			public int[] i;
+
+			public char c;
+		}
+
+		public unsafe struct TestCustomStruct3
+		{
+			public string s;
+
+			public char c;
+		}
+
+		public unsafe struct TestCustomStruct4
+		{
+			public TestRefType refType;
+
+			public char c;
+		}
+	}
+}
