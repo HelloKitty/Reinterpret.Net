bcdbc4b977ac5b0f901433872eecc9b2b7e7c61e
HelloKitty <andrew.blakely@ymail.com>
2017-09-20 13:52:01 -0500

    Updated myget build with ILRepacking

--- MyGet.bat
+++ MyGet.bat
@@ -1,2 +1,11 @@
-%NUGET% restore TypeSafe.HTTP.NET.sln -NoCache -NonInteractive -ConfigFile Nuget.config
-msbuild TypeSafe.HTTP.NET.sln /p:Configuration=Release
+%NUGET% restore Reinterpret.Net.sln -NoCache -NonInteractive
+msbuild Reinterpret.Net.sln /p:Configuration=Release
+
+ILRepack.exe /out:.\src\Reinterpret.Net\bin\Release\net20\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net20\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net20\PointerHelper.Netframework.dll .\src\Reinterpret.Net\bin\Release\net20\UnsafeAs.Backport.dll
+ILRepack.exe /out:.\src\Reinterpret.Net\bin\Release\net30\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net30\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net30\PointerHelper.Netframework.dll .\src\Reinterpret.Net\bin\Release\net30\UnsafeAs.Backport.dll
+ILRepack.exe /out:.\src\Reinterpret.Net\bin\Release\net35\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net35\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net35\PointerHelper.Netframework.dll .\src\Reinterpret.Net\bin\Release\net35\UnsafeAs.Backport.dll
+ILRepack.exe /out:.\src\Reinterpret.Net\bin\Release\net40\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net40\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net40\PointerHelper.Netframework.dll .\src\Reinterpret.Net\bin\Release\net40\UnsafeAs.Backport.dll
+ILRepack.exe /out:.\src\Reinterpret.Net\bin\Release\net451\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net451\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net451\PointerHelper.Netframework.dll .\src\Reinterpret.Net\bin\Release\net451\UnsafeAs.Backport.dll
+ILRepack.exe /out:.\src\Reinterpret.Net\bin\Release\net46\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net46\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\net46\PointerHelper.Netframework.dll
+ILRepack.exe /out:.\src\Reinterpret.Net\bin\Release\netstandard1.1\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\netstandard1.1\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\netstandard1.1\PointerHelper.Core.dll
+ILRepack.exe /out:.\src\Reinterpret.Net\bin\Release\netstandard2.0\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\netstandard2.0\Reinterpret.Net.dll .\src\Reinterpret.Net\bin\Release\netstandard12.0\PointerHelper.Core.dll
